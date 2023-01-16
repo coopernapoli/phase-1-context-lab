4b13e26c27409252dd0c94cfbc9e0c3102b789e5
Sean <napolisc10@gmail.com>
2023-01-16 12:39:19 -0700

    commit tree 0ae10c9d5586eb807b15d2e6451e0b179b739830

--- index.js
+++ index.js
@@ -21,3 +21,60 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(inputArray) {
+    return {
+        firstName: inputArray[0],
+        familyName: inputArray[1],
+        title: inputArray[2],
+        payPerHour: inputArray[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    };
+}
+
+function createEmployeeRecords(inputArray) {
+    return inputArray.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(datetime) {
+    this.timeInEvents.push({
+        type: 'TimeIn',
+        date: datetime.substr(0, 10),
+        hour: datetime.substr(11, 2),
+    });
+    return this;
+}
+
+function createTimeOutEvent(datetime) {
+    this.timeOutEvents.push({
+        type: 'TimeOut',
+        date: datetime.substr(0, 10),
+        hour: datetime.substr(11, 2),
+    });
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    const timeIn = this.timeInEvents.find(event => event.date === date);
+    const timeOut = this.timeOutEvents.find(event => event.date === date);
+
+    if (!timeIn || !timeOut) {
+        return 0;
+    }
+
+    return (timeOut.hour - timeIn.hour);
+}
+
+function wagesEarnedOnDate(date) {
+    return this.hoursWorkedOnDate(date) * this.payPerHour;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employee => employee.firstName === firstName);
+}
+
+function calculatePayroll(srcArray) {
+    return srcArray.reduce((total, employee) => total + employee.allWagesFor(), 0);
+}
+
+
